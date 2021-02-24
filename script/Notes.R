891e8202308f5456b8250f64f0f60407a9ecc3ee
Jonathan Ryan <jonathanryan576@gmail.com>
2021-02-24 07:47:52 +0700

    Initial Commit

---
+++ script/Notes.R
@@ -0,0 +1,12 @@
+""" Entah mengapa data q4 tahun 2019 kok gede banget. Ada apa ini? Kenapa
+diantara bulan Okt sampe Des 2019 banyak sekali orang yang cerai? Musti
+diselidiki!
+
+Atau jangan2 datanya salah, data yang seharusnya menampilkan q4 malah
+menampilkan data 1 tahun.
+
+Untuk sementara ini sambil menunggu balasan, lanjut kerjain project lain aja.
+Nanti klo udh tau data yang sebenernya bisa cari data per kabupaten untuk 2019.
+
+Abis itu liat perkembangannya dari 2018-2020.
+"""
